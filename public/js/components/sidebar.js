1e066ab749f57dd23da6ebdf02c82bb30c30b17e
cello-marcelino <marcelinocello027@gmail.com>
2026-08-14 00:47:29 +0700

    Fix logout URL in topbar

--- public/js/components/sidebar.js
+++ public/js/components/sidebar.js
@@ -60,7 +60,7 @@ class SidebarComponent {
                             <svg width="22" height="22" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
                             <span>Profil</span>
                         </a>
-                        <a href="/api/auth/logout" style="color: var(--danger);">
+                        <a href="/logout" style="color: var(--danger);">
                             <svg width="22" height="22" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
                             <span>Logout</span>
                         </a>
